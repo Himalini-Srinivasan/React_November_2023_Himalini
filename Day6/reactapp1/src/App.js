4e6a9b5447312919bfb9d991e0653992af3134af
Himalini-Srinivasan <150216477+Himalini-Srinivasan@users.noreply.github.com>
2023-11-23 12:36:14 +0530

    Day6

---
+++ Day6/reactapp1/src/App.js
@@ -0,0 +1,14 @@
+import Person1 from './components/Person1';
+import Person2 from './components/Person2';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Person1/>
+      <Person2/>
+    </div>
+  );
+}
+
+export default App;
